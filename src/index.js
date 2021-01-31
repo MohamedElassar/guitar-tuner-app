8afa305633234cf48d21d45fccce47d2302fa027
Mohamed Elassar <mohamedelassar1997@gmail.com>
2021-01-30 23:31:13 -0500

    Initial Commit
    Starting a new project - an onlne guitar tuner app!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class TuingOptions extends React.Component {
+    render(){
+        return(
+            <button type="button" id={this.props.id} onClick={() => this.props.onClick(this.props.id)}>
+                {this.props.tuning_name}
+            </button>
+        );
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class TuningDisplay extends React.Component {
+    render(){
+        return(
+            <button type="button" id={this.props.id}>{this.props.note}</button>
+        );
+    }
+}
+
+class Tuning extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            tuning: ["E","A","D","G","B","E"],
+            tuning_options: [
+                { name: "Standard", tuning: ["E","A","D","G","B","E"] } ,
+                { name: "Drop D", tuning: ["D","A","D","G","B","E"] } ,
+                { name: "Drop C", tuning: ["C","G","C","F","A","D"] } ,
+                { name: "Open D", tuning: ["D","A","D","F#","A","D"] } ,
+                { name: "Open C", tuning: ["C","G","C","G","C","E"] }
+            ]
+        }
+    }
+
+    handleTuningOptionClick(i){
+        let selected_tuning = this.state.tuning_options[i];
+        this.setState(
+            {
+                tuning: selected_tuning.tuning
+            }
+        );
+    }
+
+    render(){
+        const tuning_notes = this.state.tuning.map((value,index) => 
+           <TuningDisplay note={value} id={index} /> 
+        );
+
+        const tuning_options = this.state.tuning_options.map((value, index) =>
+            <TuingOptions tuning_name={value.name} id={index} onClick={(i) => this.handleTuningOptionClick(i)}/>
+        );
+
+        console.log(tuning_notes);
+
+        return (
+            <div>
+                {tuning_notes}
+                <br></br>
+                {tuning_options}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Tuning></Tuning>, document.getElementById("root"));
