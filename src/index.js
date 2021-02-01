b9c5d53418fa065a22f40d584a5a5f10cfef9d04
Mohamed Elassar <mohamedelassar1997@gmail.com>
2021-01-31 22:21:24 -0500

    Added image and lined up the note buttons (ugly)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../src/index.css';
+import guitarPic from '../src/1-front.jpg';
 
+//component to render the buttons for the different tuning options
 class TuingOptions extends React.Component {
     render(){
         return(
@@ -11,10 +14,11 @@ class TuingOptions extends React.Component {
     }
 }
 
+//component to render the notes in the selected tuning; starts with the default standard tuning
 class TuningDisplay extends React.Component {
     render(){
         return(
-            <button type="button" id={this.props.id} onClick={() => this.props.onClick(this.props.id)}>
+            <button className="notes" type="button" id={"note-" + this.props.id} onClick={() => this.props.onClick(this.props.id)}>
                 {this.props.note}
             </button>
         );
@@ -37,6 +41,7 @@ class Tuning extends React.Component {
         }
     }
 
+    //method to play the audio file when a certain note is clicked on. audio files are stored in the public folder
     handleTuningNoteClick(i){
         let current_tuning_name = this.state.current_tuning.name;
         let audio_source = "/audio-files/" + current_tuning_name + "/" + i.toString() + ".mp3";
@@ -44,6 +49,7 @@ class Tuning extends React.Component {
         audio.play();
     }
 
+    //method to handle the selection of a tuning option
     handleTuningOptionClick(i){
         let new_tuning = this.state.tuning_options[i].name;
         let new_notes = this.state.tuning_options[i].tuning;
@@ -56,16 +62,17 @@ class Tuning extends React.Component {
 
     render(){
         const tuning_notes = this.state.current_tuning.tuning.map((value,index) => 
-           <TuningDisplay note={value} id={index} onClick={(i) => this.handleTuningNoteClick(i)}/> 
+           <TuningDisplay key={index} note={value} id={index} onClick={(i) => this.handleTuningNoteClick(i)}/> 
         );
 
         const tuning_options = this.state.tuning_options.map((value, index) =>
-            <TuingOptions tuning_name={value.name} id={index} onClick={(i) => this.handleTuningOptionClick(i)}/>
+            <TuingOptions key={index} tuning_name={value.name} id={index} onClick={(i) => this.handleTuningOptionClick(i)}/>
         );
 
         return (
             <div>
-                <div>
+                <div id="container-strings">
+                    <img src={guitarPic} alt="Guitar Strings" id="guitar-string-picture"></img>
                     {tuning_notes}
                 </div> 
                 <div>
@@ -77,15 +84,16 @@ class Tuning extends React.Component {
 }
 
 
-
+//component to render the note detector section of the website
 class NoteDetector extends React.Component {
 
+    //method to access user's microphone and obtain the audio file of them playing a note
     async getAudioPermission(){
         let stream = null;
         try {
             stream = await navigator.mediaDevices.getUserMedia({audio: true, video: false});
             
-            window.streamReference = stream;
+            window.streamReference = stream;//this is done to handle the red dot that browsers display when recording audio
 
             const mediaRecorder = new MediaRecorder(stream);
 
@@ -105,6 +113,7 @@ class NoteDetector extends React.Component {
             }
 
             mediaRecorder.onstop = function(e){
+
                 if (!window.streamReference) return;
 
                 window.streamReference.getAudioTracks().forEach(function(track) {
@@ -112,6 +121,7 @@ class NoteDetector extends React.Component {
                 });
             
                 window.streamReference = null;
+
             } 
         } catch(err){
             console.log("The following getUserMedia error occured: " + err);
